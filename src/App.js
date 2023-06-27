35f6011f05af7022d72ae39dd2988a44985bf99d
chrisabedi <cabedi02@gmail.com>
2023-06-27 15:49:59 -0600

    dev: clean up

--- src/App.js
+++ src/App.js
@@ -3,66 +3,41 @@ import { Canvas } from '@react-three/fiber'
 import ColliderBox from './components/ColliderBox';
 import { Physics } from "@react-three/cannon";
 import Ground from './components/Ground'
-import { OrbitControls,PointerLockControls, Sky } from '@react-three/drei'
-import { Bike } from './components/Bike'
+import {  Sky } from '@react-three/drei'
 import { Suspense, useState, useMemo, useRef, useEffect } from "react";
-import Wakeup from './components/Wakeup'
-import Texting from './components/Texting'
 import * as THREE from 'three';
-import { useFrame } from '@react-three/fiber'
 import { CameraControls } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
 import Basic from './components/Basic'
+import {Stats} from '@react-three/drei'
 
-function CameraHelper(){
-  const camera = new THREE.PerspectiveCamera(60,1,1,3);
-  return <group position={[0,1,1]}>
-    <cameraHelper args={[camera]}/>
-  </group>
-}
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 
 export const App = () => {
 const cameraControlRef = useRef()
-
-const [action, setAction] = useState("Idle.001")
-  const [zoom, setZoom] = useState(false)
+const [action, setAction] = useState('Idle.001')
 
   return (
   <>
       <Canvas shadows camera={{ position: [-0.7, 3, 2],fov:70 }}>
-        <CameraControls ref={cameraControlRef}/>
+        <CameraControls ref={cameraControlRef} setTarget={[-.07,2,1]}/>
 
         <Sky sunPosition={[100, 20, 100]} />
         <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
         <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
         <Physics>
-          <color attach="background" args={["white"]} />
+          <color attach="background" args={["black"]} />
           <pointLight position={[-20, 10, 25]} />
           <ColliderBox position={[0, .5, 1]} scale={[0.33, 1, 0.33]} />
           <Ground />
           <Suspense fallback={null}>
             <Basic action={action} position={[0,0,1]}/>
           </Suspense>
-
         </Physics>
+        <Stats style={{ width: '160px', height: '96px', position: 'absolute', bottom: '0' }}/>
 
       </Canvas>
-      <div style={{ position: 'absolute', top: '0' }}>
-      <button
-        type="button"
-        onClick={() => {
-          cameraControlRef.current?.rotate(Math.PI/4, 0, true);
-        }}
-      >rotate</button>
-      	<button
-					type="button"
-					onClick={() => {
-						cameraControlRef.current?.reset(true);
-					}}
-				>
-					reset
-				</button>
+      <div style={{ position: 'absolute', bottom: '0', right:'0'}}>
         <button
         type="button"
         onClick={() => {
@@ -105,4 +80,3 @@ const [action, setAction] = useState("Idle.001")
       );
 }
 
-//      <FloorPlane position={[-2.0, .7, -1]}/>
